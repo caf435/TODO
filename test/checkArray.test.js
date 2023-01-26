a13d804fa3e9d203e1817019aa26c275a69a1c1b
caf435 <caftales@gmail.com>
2023-01-26 21:54:24 +0900

    release 2

--- test/checkArray.test.js
+++ test/checkArray.test.js
@@ -2,30 +2,34 @@ const processArray = require("../index");
 
 //функция где название тест
 describe("Тесты функции обработки массива", () => {
+  let array;
+  beforeAll(() => {
+    array = [];
+  });
   //тесты
   describe("Общие тесты", () => {
     test("Возвращает ли наша функция массив?", () => {
-      const array = [];
       expect(Array.isArray(processArray(array))).toBe(true);
     });
     test("Возвращает ли функция пустой массив при передаче его в эту функцию?", () => {
-      const array = [];
       expect(processArray(array)).toEqual([]);
     });
   });
 
   describe("Функциональные тесты", () => {
+    beforeEach(() => {
+      array = [1, 2, 1, 2, 2, 3, 4];
+    });
     test("Удаляет ли она повторяющиеся значения?", () => {
-      const array = [1, 2, 1, 2, 2, 3, 4];
       expect(processArray(array)).toEqual([1, 2, 3, 4]);
     });
     test("Заменяет ли функция отрицательные значения на *?", () => {
-      const array = [1, 2, 1, -2, 2, 3, 4];
+      array.push(-2);
       expect(processArray(array)).toContain("*");
     });
     test("Срабатывает ли наша функция на всех отрицательных значениях на *?", () => {
-      const array = [1, 2, 1, -2, 2, -3, 4];
-      expect(processArray(array)).toEqual([1, 2, "*", "*", 4]);
+      array.push(...[-2, 4, -3]);
+      expect(processArray(array)).toEqual([1, 2, 3, 4, "*", "*"]);
     });
   });
 });
