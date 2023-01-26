fc9fc142bacdc5b823d50e741b6685af11dc9397
caf435 <caftales@gmail.com>
2023-01-26 21:45:12 +0900

    release 1

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function processArray(array) {
-  return [];
+  if (!array.length) {
+    return [];
+  }
+  let uniqueArray = Array.from(new Set(array));
+  uniqueArray = uniqueArray.map((el) => (el >= 0 ? el : "*"));
+  return uniqueArray;
 }
 
 module.exports = processArray;
--- test/checkArray.test.js
+++ test/checkArray.test.js
@@ -8,9 +8,24 @@ describe("Тесты функции обработки массива", () => {
       const array = [];
       expect(Array.isArray(processArray(array))).toBe(true);
     });
+    test("Возвращает ли функция пустой массив при передаче его в эту функцию?", () => {
+      const array = [];
+      expect(processArray(array)).toEqual([]);
+    });
   });
 
-  //   describe("Функциональные тесты", () => {
-  //     test("Удаляет ли она повторяющиеся значения?", () => {});
-  //   });
+  describe("Функциональные тесты", () => {
+    test("Удаляет ли она повторяющиеся значения?", () => {
+      const array = [1, 2, 1, 2, 2, 3, 4];
+      expect(processArray(array)).toEqual([1, 2, 3, 4]);
+    });
+    test("Заменяет ли функция отрицательные значения на *?", () => {
+      const array = [1, 2, 1, -2, 2, 3, 4];
+      expect(processArray(array)).toContain("*");
+    });
+    test("Срабатывает ли наша функция на всех отрицательных значениях на *?", () => {
+      const array = [1, 2, 1, -2, 2, -3, 4];
+      expect(processArray(array)).toEqual([1, 2, "*", "*", 4]);
+    });
+  });
 });
